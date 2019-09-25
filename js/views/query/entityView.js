9e4ceb082f02aafb55605cfc5b7c28cf564eb65f
(Noah) Hanlun Song <hsong@air.org>
2019-09-25 14:35:54 -0400

    reuse header and footer from PatentsView-API

--- js/views/query/entityView.js
+++ js/views/query/entityView.js
@@ -3,36 +3,30 @@
 /**************************************************************/
 
 define([
-    'jquery',
-    'underscore',
-    'backbone',
-    'handlebars',
-    'text!../../../templates/query/entity.html'
-], function ($, _, Backbone, Handlebars, entityTemplate) {
-
+    "jquery",
+    "underscore",
+    "backbone",
+    "handlebars",
+    "text!../../../templates/query/entity.html"
+], function($, _, Backbone, Handlebars, entityTemplate) {
     var EntityView = Backbone.View.extend({
-        tagName: 'div',
-        initialize: function () {
-
-            _.bindAll(this, 'render', 'updateModel');
-            this.template = Handlebars.compile(entityTemplate);
+        tagName: "div",
+        initialize: function() {
+            _.bindAll(this, "render", "updateModel")
+            this.template = Handlebars.compile(entityTemplate)
         },
         events: {
             "click #step .btn-previous": "updateModel",
             "click #step .btn-next": "updateModel"
         },
-        render: function () {
-            debugger;
-            var json = JSON.parse(JSON.stringify(this.model.toJSON()));
-            $(this.el).empty();
-            $(this.el).append(this.template(json));
-            return this;
+        render: function() {
+            var json = JSON.parse(JSON.stringify(this.model.toJSON()))
+            $(this.el).empty()
+            $(this.el).append(this.template(json))
+            return this
         },
-        updateModel: function () {
+        updateModel: function() {}
+    })
 
-        }
-    });
-
-    return EntityView;
-
-});
+    return EntityView
+})
