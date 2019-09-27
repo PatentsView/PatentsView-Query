75dc2214c9cfa37a84aaf75bf6923733f9a5e4f1
(Noah) Hanlun Song <hsong@air.org>
2019-09-27 14:10:04 -0400

    integrate new header and footer

--- query/thank-you.php
+++ query/thank-you.php
@@ -48,34 +48,7 @@ pclose(popen('start /B php.exe C:/querytool/backend.php '.$id.' 2>nul >nul', "r"
         <a href="tel:1-800-786-9199" title="USPTO Customer Support Toll-Free Phone Number">1-800-786-9199</a>.
     </noscript>
     <div class="page-wrapper">
-        <header class="med-header">
-            <div>
-                <h1>
-                    <a href="/" title="PatentsView - USPTO">
-                        <img alt="PatentsView Logo" src="../assets/img/logo_2x.png" style="width: 221px; margin-top: 6px;" />
-                    </a>
-                </h1>
-                <nav>
-                    <ul>
-                        <li class="api">
-                            <a href="http://www.patentsview.org/api/doc.html">
-                                <img class="logo-img" alt="API" src="../assets/img/shared/api.png" />API
-                            </a>
-                        </li>
-                        <li class="data-query active">
-                            <a href="http://www.patentsview.org/query/">
-                                <img class="logo-img" alt="API" src="../assets/img/shared/data-query.png" />Data Query
-                            </a>
-                        </li>
-                        <li class="data-download">
-                            <a href="http://www.patentsview.org/download/">
-                                <img class="logo-img" alt="API" src="../assets/img/shared/data-download.png" />Data Download
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
+        
         <!-- Container -->
         <section id="step-by-step-container" class="container"></section>
         <!-- /Container -->
@@ -100,99 +73,6 @@ pclose(popen('start /B php.exe C:/querytool/backend.php '.$id.' 2>nul >nul', "r"
                 </div>
             </div>
     </div>
-    <footer class="fat-footer primary-footer">
-        <div class="center-shell">
-            <div class="column one">
-                <div class="new-info">
-                    <h4>PatentsView Info</h4>
-                    <ul>
-                        <li class="relationships">
-                            <a href="#viz/relationships" title="Relationships">Relationships</a>
-                        </li>
-                        <li class="locations-link">
-                            <a href="#viz/locations" title="Locations">Locations</a>
-                        </li>
-                        <li class="comparisons">
-                            <a href="#viz/comparisons" title="Comparisons">Comparisons</a>
-                        </li>
-                        <li class="list-search">
-                            <a href="#search&amp;simp=1" title="List Search">List Search</a>
-                        </li>
-                        <li class="about">
-                            <a href="#" title="About">About</a>
-                        </li>
-                        <li class="methods">
-                            <a href="#" title="Methods and Sources">Methods and Sources</a>
-                        </li>
-                        <li class="glossary">
-                            <a href="/api/glossary.html" title="Glossary" style="text-decoration: underline;">Glossary</a>
-                        </li>
-                        <li class="feedback">
-                            <a href="mailto:cssip@air.org" title="Send Feedback">Send Feedback</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="copyright">
-                    <!-- react-text: 127 -->© Copyright<!-- /react-text --><!-- react-text: 128 --><!-- /react-text --><!-- react-text: 129 -->2017<!-- /react-text -->
-                </div>
-            </div>
-            <div class="column two">
-                <div class="new-data-sources">
-                    <h4>Data Sources</h4>
-                    <ul>
-                        <li>
-                            <a href="/api">Api</a>
-                        </li>
-                        <li>
-                            <a href="/query">Data Query</a>
-                        </li>
-                        <li>
-                            <a href="/download">Data Download</a>
-                        </li>
-                        <li>
-                            <a href="http://www.patentsview.org/community">Community</a>
-                        </li>
-                    </ul>
-                    <div class="update-date">
-                        <!-- react-text: 143 -->Data as of <!-- /react-text --><!-- react-text: 144 -->August 08, 2017<!-- /react-text -->
-                    </div>
-                </div>
-            </div>
-            <div class="column three">
-                <div class="new-credit">
-                    <h4>This Initiative</h4>
-                    <p>This platform uses data derived from the US Patent and Trademark Office (USPTO) bulk data files. These data are provided for research purposes and do not constitute the official USPTO record.</p>
-                    <p>
-                        <!-- react-text: 150 -->Supported by the<!-- /react-text --><!-- react-text: 151 --><!-- /react-text -->
-                        <a href="http://www.uspto.gov/economics/" target="_blank">USPTO Office of the Chief Economist</a><!-- react-text: 153 -->, with additional support from the<!-- /react-text --><!-- react-text: 154 --><!-- /react-text -->
-                        <a href="http://www.usda.gov" target="_blank">US Department of Agriculture</a><!-- react-text: 156 --><!-- /react-text --><!-- react-text: 157 -->(USDA).<!-- /react-text -->
-                    </p>
-                </div>
-                <div class="logo-link">
-                    <a href="http://www.uspto.gov/" target="_blank">
-                        <img class="logo-img" alt="sponsored by the USPTO" src="../assets/img/landing/uspto.png" />
-                    </a>
-                </div>
-            </div>
-            <div class="column four">
-                <div class="terms-and-services">
-                    <h4>Terms &amp; Services</h4>
-                    <p>
-                        <!-- react-text: 165 -->Users are free to use, share, or adapt the material for any purpose, subject to the standards of the<!-- /react-text --><!-- react-text: 166 --><!-- /react-text -->
-                        <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank">Creative Commons Attribution 4.0 International License</a><!-- react-text: 168 -->.<!-- /react-text -->
-                    </p>
-                    <p>
-                        <!-- react-text: 170 -->Attribution should be given to<!-- /react-text --><!-- react-text: 171 --><!-- /react-text -->
-                        <a href="http://www.patentsview.org/">PatentsView</a><!-- react-text: 173 --><!-- /react-text --><!-- react-text: 174 -->for use, distribution, or derivative works.<!-- /react-text -->
-                    </p>
-                </div>
-                <div class="logo-link">
-                    <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank">
-                        <img class="logo-img" alt="Creative Commons License" src="../assets/img/landing/creative-commons.png" />
-                    </a>
-                </div>
-            </div>
-        </div>
-    </footer>
+    <script src="../js/header_footer.js"></script>
 </body>
 </html>
