34872ff808d0c82751ccb0bf8e34b9c88bdab9a9
Noah <hsong@air.org>
2021-01-04 12:21:08 -0500

    refactoring

--- thank-you.php
+++ thank-you.php
@@ -41,7 +41,9 @@ if (strtoupper(substr(php_uname('s'), 0, 3)) === 'WIN') {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Create a new Query">
     <meta name="author" content="AIR">
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,400italic,600,700,800%7CRoboto+Slab:400,700' rel='stylesheet' type='text/css'>
+    <!-- <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,400italic,600,700,800%7CRoboto+Slab:400,700' rel='stylesheet' type='text/css'> -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/headfoot.css" />
     <link rel="stylesheet" href="assets/css/font-awesome.min.css" />
     <link rel="stylesheet" href="assets/css/pvqt.css" />
